d6e583ef138f240e6bff35c0a1648371c579b2a4
Josh Ospina Tafur <Josh.999126@gmail.com>
2025-07-14 21:29:57 -0500

    Fix plugins user permissions

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+import crypto from 'crypto'
+
+export default ({ env }) => ({
+  'user-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
+      jwt: {
+        expiresIn: '30d',
+      },
+    },
+  },
+})
